48b55f5c9d39095d3a048f73d02f1ab865cec7e4
Giada <21024993+GiadaSimonetti@users.noreply.github.com>
2019-04-05 17:23:31 +0100

    Change the shape in torus

--- js/index.js
+++ js/index.js
@@ -8,20 +8,24 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild(renderer.domElement);
 
-//  create the shape
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-//  create material, color or image texture
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00, wireframe: true } );
-const cube = new THREE.Mesh( geometry, material );
+// //  create the shape
+// const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+// //  create material, color or image texture
+// const material = new THREE.MeshBasicMaterial( { color: 0x00ff00, wireframe: true } );
+// const cube = new THREE.Mesh( geometry, material );
 
-scene.add( cube ); // till here I can't see anything becauase I need to change the position of the camera
-camera.position.z = 3;
+var geometry = new THREE.TorusGeometry( 10, 3, 16, 100 );
+var material = new THREE.MeshBasicMaterial( { color: 0xffff00, wireframe: true } );
+var torus = new THREE.Mesh( geometry, material );
+scene.add( torus );
+
+camera.position.z = 30;
 
 //  game logic
 
 const update = () => {
-    cube.rotation.x += 0.01
-    cube.rotation.y += 0.01
+    torus.rotation.x += 0.01
+    torus.rotation.y += 0.01
 }
 
 //  draw scene
