0634d2dfa4af5e78c0fac308d18f2988c8cde5ef
Giada <21024993+GiadaSimonetti@users.noreply.github.com>
2019-04-05 16:06:26 +0100

    Add scene, camera, renderer and some logic

--- js/index.js
+++ js/index.js
@@ -1,5 +1,30 @@
 alert("BOOOOOOOOOOM 👾")
 // scene is what you've been viewing
-let scene = THREE.Scene();
+let scene = new THREE.Scene();
 //  camer is what user can see
-let camera = THREE.PerspectiveCamera();
+let camera = new THREE.PerspectiveCamera(75, window.innereWidth / window.innerHeight, 0.1, 1000);
+
+let renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innereWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+//  game logic
+
+const update = () => {
+
+}
+
+//  draw scene
+const render = () => {
+    renderer.render(scene, camera);
+
+}
+
+//  run GameLoop(update, render,repeat)
+const GameLoop = () => {
+    requestAnimationFrame(GameLoop);
+    update();
+    render();
+}
+
+GameLoop();
