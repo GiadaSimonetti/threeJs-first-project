82188736d5e23bc99a3c670ab714705b4f6450ec
Giada <21024993+GiadaSimonetti@users.noreply.github.com>
2019-04-05 18:33:16 +0100

    Add controls system

--- js/index.js
+++ js/index.js
@@ -1,24 +1,29 @@
 alert( "BOOOOOOOOOOM 👾" )
+
+let width = window.innerWidth;
+let height = window.innerHeight;
+
 // scene is what you've been viewing
 const scene = new THREE.Scene();
 //  camera is what user can see
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera( 75, width  / height, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setSize( width , height );
 document.body.appendChild(renderer.domElement);
 
 window.addEventListener( 'resize', function(){
-    const width = window.innerWidth;
-    const height = window.innerHeight;
-    renderer.setSize( width, height );
+    width = window.innerWidth;
+    height = window.innerHeight;
     camera.aspect = width / height ;
     camera.updateProjectionMatrix();
 });
 
-var geometry = new THREE.TorusGeometry( 10, 3, 16, 100 );
-var material = new THREE.MeshBasicMaterial( { color: 0xffff00, wireframe: true } );
-var torus = new THREE.Mesh( geometry, material );
+controls = new THREE.OrbitControls( camera, renderer.domElement );
+
+const geometry = new THREE.TorusGeometry( 10, 3, 16, 100 );
+const material = new THREE.MeshBasicMaterial( { color: 0xffff00, wireframe: true } );
+const torus = new THREE.Mesh( geometry, material );
 scene.add( torus );
 
 camera.position.z = 30;
@@ -26,8 +31,8 @@ camera.position.z = 30;
 //  game logic
 
 const update = () => {
-    torus.rotation.x += 0.01
-    torus.rotation.y += 0.01
+    // torus.rotation.x += 0.01
+    // torus.rotation.y += 0.01
 }
 
 //  draw scene
